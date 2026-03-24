d9fab39bdc3c9baec69aa2a67d276245893d59ab
kwlcode <106566430+kwlcode@users.noreply.github.com>
2026-03-25 01:10:02 +0200

    Initial Commit

---
+++ ContactsOverlayApp/ContactsOverlayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsOverlayAppApp.swift
+//  ContactsOverlayApp
+//
+//  Created by NOWMEDIA-AI on 2026/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactsOverlayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
